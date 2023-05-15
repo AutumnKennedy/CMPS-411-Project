c190035ee7144fb2746f4b46ad247b6d8365778c
Autumn Kennedy <autumnkennedy0714@gmail.com>
2023-05-14 22:48:33 -0500

    Added Microsoft Identity, created dbContext, and created ShelterDto

---
+++ HelpfulNeighbor.web/Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using HelpfulNeighbor.web.Features.Shelters;
+using HelpfulNeighbor.web.Features.Shelters.ShelterType;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HelpfulNeighbor.web.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Shelter> Shelter { get; set; }
+        public DbSet<ShelterType> ShelterType { get; set; }
+
+        public DataContext() 
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
+
+    }
+}
---
+++ HelpfulNeighbor.web/Features/Shelters/Shelter.cs
@@ -0,0 +1,12 @@
+﻿namespace HelpfulNeighbor.web.Features.Shelters
+{
+    public class Shelter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+
+    }
+}
