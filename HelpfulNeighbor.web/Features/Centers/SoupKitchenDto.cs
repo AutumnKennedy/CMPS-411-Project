f6cf0072a51db9c4a81b3c61ba708bc503804561
Kaitlyn Hayes <kaitlyn.hayes-2@selu.edu>
2023-05-20 01:22:00 -0500

    added onto the dto for soup kitchen

--- HelpfulNeighbor.web/Features/Centers/SoupKitchenDto.cs
+++ HelpfulNeighbor.web/Features/Centers/SoupKitchenDto.cs
@@ -8,4 +8,45 @@
         public string HoursOfOperation { get; set; }
         public int TypeId { get; set; }
     }
+
+    public class SoupKitchenGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class SoupKitchenCreateDto
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class SoupKitchenUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class SoupKitchenListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class SoupKitchenOptionDto
+    {
+        public string text { get; set; }
+        public string value { get; set; }
+    }
 }
