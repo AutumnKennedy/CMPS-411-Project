5a5590696422cae9d85502a3915530ff3f689a39
Kaitlyn Hayes <kaitlyn.hayes-2@selu.edu>
2023-05-20 01:29:37 -0500

    added missing code to the center type for donation center

--- HelpfulNeighbor.web/Features/Centers/CenterType/DonationCenterTypeDto.cs
+++ HelpfulNeighbor.web/Features/Centers/CenterType/DonationCenterTypeDto.cs
@@ -1,6 +1,13 @@
-﻿namespace HelpfulNeighbor.web.Features.Centers.CenterType
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HelpfulNeighbor.web.Features.Centers.CenterType
 {
     public class DonationCenterTypeDto
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [NotNull]
+        public string Type { get; set; } = string.Empty;
     }
 }
