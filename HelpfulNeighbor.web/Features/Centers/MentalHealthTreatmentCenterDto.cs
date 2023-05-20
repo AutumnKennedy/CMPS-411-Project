e897b164705f2eb7a1e3b56e57543a178087dfa8
Kaitlyn Hayes <kaitlyn.hayes-2@selu.edu>
2023-05-20 00:43:52 -0500

    expanded on mental health dto

--- HelpfulNeighbor.web/Features/Centers/MentalHealthTreatmentCenterDto.cs
+++ HelpfulNeighbor.web/Features/Centers/MentalHealthTreatmentCenterDto.cs
@@ -8,4 +8,45 @@
         public string HoursOfOperation { get; set; }
         public int TypeId { get; set; }
     }
+
+    public class MentalHealthTreatmentCenterGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class MentalHealthTreatmentCenterCreateDto
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class MentalHealthTreatmentCenterUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class MentalHealthTreatmentCenterListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string HoursOfOperation { get; set; }
+        public int TypeId { get; set; }
+    }
+
+    public class MentalHealthTreatmentCenterOptionDto
+    {
+        public string text { get; set; }
+        public string value { get; set; }
+    }
 }
